e20d091749dd6d1357b30e0c5e2064c74a8f8a64
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-05-10 13:35:13 +0530

    changes

--- api/controllers/OtpController.js
+++ api/controllers/OtpController.js
@@ -29,20 +29,20 @@ module.exports = {
 			}
 	},
 
-	getNumber: function(req, res) {
+	checkOtp: function(req, res) {
 			if (req.body) {
 					if (req.body.contact && req.body.contact != "") {
-							Otp.getNumber(req.body, function(err, data) {
+							Otp.checkOtp(req.body, function(err, data) {
 									if (err) {
 											res.json({
 													value: false,
 													data: err
 											});
 									} else {
-										req.session.userId = data._id;
-										// data.otp = (Math.random()+"").substring(2,8);
-										// data.modificationDate = Date();
-										console.log(req.session.userId);
+										// req.session.userId = data._id;
+										// // data.otp = (Math.random()+"").substring(2,8);
+										// // data.modificationDate = Date();
+										// console.log(req.session.userId);
 											res.json({
 													value: true,
 													data: data
--- api/services/Otp.js
+++ api/services/Otp.js
@@ -4,71 +4,62 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
- var mongoose = require("mongoose");
- var Schema = mongoose.Schema;
- var schema = new Schema({
-     contact: String,
-     otp: String,
-     timestamp: {
-         type: Date,
-         default: Date.now
-     }
-   });
-   module.exports = mongoose.model("Otp", schema);
-   var model = {
-       saveData: function(data, callback) {
-           var otp = this(data);
-          //  if (data._id) {
-          //    data.modificationDate = new Date();
-          //      this.findOneAndUpdate({
-          //          _id: data._id
-          //      }, data, function(err, data2) {
-          //          if (err) {
-          //              console.log(err);
-          //              callback(err, null);
-          //          } else {
-          //              callback(null, data2);
-          //          }
-          //      });
-          //  } else {
- otp.otp = (Math.random() + "").substring(2, 8);
-               otp.save(function(err, data2) {
-                   if (err) {
-                       console.log(err);
-                       callback(err, null);
-                   } else {
-                       callback(null, data2);
-                   }
-               });
-          //  }
-       },
-
-           getNumber: function(data, callback) {
-               otp.findOne({
-                   contact: data.contact
-               }, function(err, data2) {
-                   if (err) {
-                       console.log(err);
-                       callback(err, null);
-                   } else {
-                       if (data2 == null) {
-                           // console.log("no data");
-                           User.saveData(data, function(err, data3) {
-                               if (err) {
-                                   console.log(err);
-                                   callback(err, null);
-                               } else {
-                                   callback(err, data3);
-                               }
-                           });
-
-                       } else {
-                           callback(null, data2);
-                       }
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var schema = new Schema({
+    contact: String,
+    otp: String,
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+var d = new Date();
+d.setMinutes(d.getMinutes() - 10);
+module.exports = mongoose.model("Otp", schema);
+var model = {
+        saveData: function(data, callback) {
+            var otp = this(data);
+            otp.otp = (Math.random() + "").substring(2, 8);
+            otp.save(function(err, data2) {
+                if (err) {
+                    console.log(err);
+                    callback(err, null);
+                } else {
+                    callback(null, data2);
+                }
+            });
+        },
 
 
-                   }
-               });
-           },
-     };
-     module.exports = _.assign(module.exports, model);
+        checkOtp: function(data, callback) {
+            Otp.findOne({
+                    contact: data.contact,
+                    otp: data.otp,
+                    timestamp: {
+                        $gte: d
+                    }
+                }, function(err, data2) {
+                    if (err) {
+                        console.log(err);
+                        callback(err, null);
+                    } else {
+                        if (data2 != null) {
+                            console.log("in data 2");
+                            console.log(data2.timestamp);
+                            User.saveData(data, function(err, data3) {
+                            if (err) {
+                                console.log(err);
+                                callback(err, null);
+                            } else {
+                                callback(err, data3);
+                            }
+                        });
+                            console.log("after save data");
+                    }
+                    // callback(null, data2);
+                }
+            });
+    },
+};
+module.exports = _.assign(module.exports, model);
